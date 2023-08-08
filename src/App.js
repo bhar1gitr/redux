485dbb0611b4ea8ecb89c5f68733397857237819
Bharat Sharma <97161064+bhar1gitr@users.noreply.github.com>
2023-08-08 16:35:10 +0000

    fourth commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { decrement, increment } from './Features/Counter/counters'
+import "./App.css";
 
 const App = () => {
   const count = useSelector(state => state.counter.value)
   const dispatch = useDispatch()
 
   return (
-    <div>
+    <div className='main'>
+
+      <h1>Counter</h1>
+
       <div>
-        <button
+        <button className='btn'
           aria-label="Increment value"
           onClick={() => dispatch(increment())}
         >
-          Increment
+          +
         </button>
         <span>{count}</span>
-        <button
+        <button className='btn'
           aria-label="Decrement value"
           onClick={() => dispatch(decrement())}
         >
-          Decrement
+          -
         </button>
       </div>
     </div>
